992eff707abf008103b9bbc8e4a11b18d8b201f3
Michias Shiferaw <michias.shiferaw@mths.ca>
2020-02-16 15:57:46 +0000

    initial commit

--- Einstein.java
+++ Einstein.java
@@ -2,7 +2,25 @@
  * Class Einstein
  */
 public class Einstein {
-	public static void main(String[] args) {
-    	System.out.println("Hello from AWS Cloud9!");
-	}
+  public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("Please enter a mass (kg).");
+      //place the user's input in a specified variable
+      double mass1 = input.nextDouble();
+
+     System.out.println("Please enter a mass (kg).");
+     //input
+     Scanner wordScanner = new Scanner(System.in);
+     //final double mass1 = wordScanner.nextDouble();
+     double speedOfLight = (299792458);
+     
+     //process
+     final double energy1 = (mass1 * speedOfLight * speedOfLight);
+     final double kilotonE = energy1 * 0.00000000000023901;
+     
+     //output
+     System.out.println("This mass releases " + energy1 + "J of energy.");
+     System.out.println( " " + mass1 + "kg can also be equivalent to" + kilotonE + "kt bombs.") ;
+  }
+  
 }
