5607a58e658b52794fd9e21a468c108c5c25e47e
Michias Shiferaw <michias.shiferaw@mths.ca>
2020-02-21 17:39:26 +0000

    initial commit

--- Einstein.java
+++ Einstein.java
@@ -1,27 +1,50 @@
-/**
- * Class Einstein
+import java.util.Scanner;
+/** 
+ * This program finds e in e=mc^2 given mass.
+ * @author Michias Shiferaw
+ * @Version 2.0
+ * @since 2020-02-18.
  */
+ 
 public class Einstein {
+  
+  /** 
+  * This program uses Einstein's infamous e=mc^2 to find energy with given mass.
+  * @author Michias Shiferaw
+  * @Version 2.0
+  * @since 2020-02-18.
+  */
   public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
-      System.out.println("Please enter a mass (kg).");
-      
-      //place the user's input in a specified variable
-      double mass1 = input.nextDouble();
-
-     System.out.println("Please enter a mass (kg).");
-     //input
-     Scanner wordScanner = new Scanner(System.in);
     
-     double speedOfLight = (299792458);
+    //Asking for question
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a mass (kg).");
+      
+    //place the user's input in a specified variable
+    double mass = input.nextDouble();
+    try { 
+      if (mass >= 0) {
+        //Recognize a constant
+        double speedOfLight = (299792458);
      
-     //process
-     final double energy1 = (mass1 * speedOfLight * speedOfLight);
-     final double kilotonE = energy1 * 0.00000000000023901;
+        //process of calulating the energy used and also converting into Kiloton bombs
+        final double energy = mass * Math.pow(299792458,2);
+        final double kilotonE =(int)( energy * 0.00000000000023901);
      
-     //output
-     System.out.println("This mass releases " + energy1 + "J of energy.");
-     System.out.println( " " + mass1 + "kg can also be equivalent to" + kilotonE + "kt bombs.") ;
+        //output
+        System.out.println("This mass releases " + energy + "J of energy.");
+        System.out.println(" " + mass + "kg can also be equivalent to " + kilotonE + "kt bombs.");
+     
+      } else {
+        System.err.println("Not valid input");
+        System.out.println("Mass can only be negative if it is being repelled by gravity.");
+      }
+    } catch (NumberFormatException nfe) {
+      System.err.println("Invalid input!");
+    }
+       
+    System.out.println();
+    System.out.println("Bye Bye!");
   }
   
 }
